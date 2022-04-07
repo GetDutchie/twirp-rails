2f5f575c159af2bee42ff4895e0ce8b310bebf8a
Evan Waters <735779+evan-waters@users.noreply.github.com>
2022-04-06 20:53:22 -0600

    Revert "Revert "v0.5.0""

--- lib/twirp/rails/helpers/hooks.rb
+++ lib/twirp/rails/helpers/hooks.rb
@@ -10,27 +10,16 @@ module Twirp
           klass.extend ClassMethods
         end
 
+        attr_reader :service_wrapper, :options
+
+        def initialize(service_wrapper, **options)
+          @service_wrapper = service_wrapper
+          @options = options
+        end
+
         module ClassMethods
-          def register_hook(namespace, key)
-            Twirp::Rails.hooks[namespace] ||= {}
-
-            if Twirp::Rails.hooks.dig(namespace, key).present?
-              raise ArgumentError.new(
-                "A hook with the key #{key} has already been registered under the #{namespace} namespace"
-              )
-            end
-
-            if Twirp::Rails.hooks[namespace].values.include?(self)
-              raise ArgumentError.new(
-                "#{self} is already registered."
-              )
-            end
-
-            Twirp::Rails.hooks[namespace][key] = self
-          end
-
-          def attach(service)
-            hook_instance = self.new
+          def attach(service_wrapper, **options)
+            hook_instance = self.new(service_wrapper, **options)
 
             unless HOOK_METHODS.any? {|method| hook_instance.respond_to?(method)}
               raise NotImplementedError.new(
@@ -39,28 +28,30 @@ module Twirp
             end
 
             if hook_instance.respond_to?(:before)
-              service.before do |rack_env, env|
+              service_wrapper.before do |rack_env, env|
                 hook_instance.before(rack_env, env)
               end
             end
 
             if hook_instance.respond_to?(:on_success)
-              service.on_success do |env|
+              service_wrapper.on_success do |env|
                 hook_instance.on_success(env)
               end
             end
 
             if hook_instance.respond_to?(:on_error)
-              service.on_error do |twerr, env|
+              service_wrapper.on_error do |twerr, env|
                 hook_instance.on_error(twerr, env)
               end
             end
 
             if hook_instance.respond_to?(:exception_raised)
-              service.exception_raised do |e, env|
+              service_wrapper.exception_raised do |e, env|
                 hook_instance.exception_raised(e, env)
               end
             end
+
+            hook_instance
           end
         end
       end
